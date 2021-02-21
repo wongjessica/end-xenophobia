7815b4cf6f7c8c3131d8494a8d068e5834320455
adeeburrahman <57969730+adeeburrahman@users.noreply.github.com>
2021-02-20 22:45:14 -0500

    add comments to unfurling code

--- js/unfurl.js
+++ js/unfurl.js
@@ -2,22 +2,22 @@ const getInformation = async (fetch, links) => {
   const linkInformations = links.map(link =>
     fetch(link)
       .then(res => res.text())
-      .then(data => data.split("\n"))
+      .then(data => data.split("\n")) // extract article html as text
       .then(lines =>
         ["title", "description", "image"].map(attribute =>
           lines.filter(item => {
-            const regex = new RegExp(`og:${attribute}`, "gi");
+            const regex = new RegExp(`og:${attribute}`, "gi"); // filter out meta tags
             return item.match(regex);
           })
         )
       )
       .then(relevantOgs => relevantOgs.map(relevantOg => relevantOg[0]))
       .then(stringifiedMetas => {
-        parser = new DOMParser();
+        parser = new DOMParser(); // create dom parser to interpret string
         return stringifiedMetas.map(stringifiedMeta =>
           parser
-            .parseFromString(stringifiedMeta, "text/xml")
-            .childNodes[0].getAttribute("content")
+            .parseFromString(stringifiedMeta, "text/xml") // interpret meta tags as single xml node
+            .childNodes[0].getAttribute("content") // extract actual content
         );
       })
   );
@@ -26,7 +26,7 @@ const getInformation = async (fetch, links) => {
 
   const formattedInformation = rawInformations.reduce(
     (stored, [title, description, image], index) => {
-      const imageLink = image.substring(image.indexOf("http"));
+      const imageLink = image.substring(image.indexOf("http")); // ensure image link is in proper format
       const information = `
         <div>
           <p><img src="${imageLink}" style="height: 7rem;width: 7rem;float: left;padding-right: 2rem;"/><p/>
